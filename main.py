b3e26504e9e909a4d021e1721e05f40f2965bcc7
Hemanth@Arch <hemanth.kotagiri43@gmail.com>
2021-05-23 12:38:53 +0530

    Add routing

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from service import Crawler
 import json
-from flask import Flask, Response, jsonify
+from flask import Flask, Response, request
 
 # Initializing the Crawler object from service
 scrapper = Crawler()
 
-# scrapper.get_result("185u1a0565", "2001-04-03", "1,1")
-# scrapper.get_result("185u1a0565", "2001-04-03", "1,2")
-# scrapper.get_result("185u1a0565", "2001-04-03", "2,1")
-
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    result = scrapper.get_result("185u1a0565", "2001-04-03", "1,1")
+@app.route("/<hallticket>/<dob>/<year>")
+def index(hallticket, dob, year):
+
+    # hallticket = request.args.get("hallticket")
+    # dob = request.args.get("dob")
+    # year = request.args.get("year")
+
+    result = scrapper.get_result(hallticket, dob, year)
+    # print(result)
     return Response(json.dumps(result),  mimetype='application/json')
 
 
